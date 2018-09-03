522194f66d05671bddea0860adb07f63ff8f2f5b
francislagueu <lagson6@yahoo.fr>
2018-09-03 02:08:53 -0700

    Added error handler middleware

--- index.js
+++ index.js
@@ -15,4 +15,21 @@ app.use(express.urlencoded({extended: false}));
 app.use(cors());
 app.use(volleyball);
 
+function notFound(req, res, next){
+    res.status(404);
+    const error = new Error('Not Found - '+ req.originalUrl);
+    next(error);
+}
+
+function errorHandler(err,req, res, next){
+    res.status(res.statusCode || 500);
+    res.json({
+        message: err.message,
+        stack: err.stack
+    });
+}
+
+app.use(notFound);
+app.use(errorHandler);
+
 app.listen(port, ()=>console.log(`Server started on port: ${port}`));
